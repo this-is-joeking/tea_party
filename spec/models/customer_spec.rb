9e60b7815490b654062acba3034efc33f926414b
Joe King <j.king92@gmail.com>
2023-04-12 13:24:59 -0700

    chore: Add validations to subscriptions and associated tests

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Customer do
   describe 'relationships' do
     it { should have_many :subscriptions }
+    it { should have_many(:teas).through(:subscriptions) }
   end
 
   describe 'validations' do
