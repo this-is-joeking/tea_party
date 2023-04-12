2900d7adce89045ca235dffca7d45d00696a6b68
Joe King <j.king92@gmail.com>
2023-04-12 14:55:00 -0700

    fix: subscription validations to validate active is true or false instead of validating presence

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -4,11 +4,10 @@ class Subscription < ApplicationRecord
 
   validates_presence_of :title,
                         :price,
-                        :active,
                         :frequency,
                         :customer_id,
                         :tea_id
-  
+  validates :active, inclusion: { in: [true, false] }
   validates_numericality_of :frequency
 
   enum frequency: { weekly: 0, biweekly: 1, monthly: 2, quarterly: 3 }
--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :subscription do
     title { Faker::Tea.variety }
     price { Faker::Number.within(range: 5.99..99.99) }
-    active { true }
+    active { Faker::Boolean.boolean(true_ratio: 0.8) }
     frequency { Faker::Number.within(range: 0..3) }
     customer_id { Faker::Number.within(range: Customer.minimum(:id)..Customer.maximum(:id)) }
     tea_id { Faker::Number.within(range: Tea.minimum(:id)..Tea.maximum(:id)) }
